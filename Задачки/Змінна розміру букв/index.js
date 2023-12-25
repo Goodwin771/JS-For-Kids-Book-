75a106cc553be9978fa2ef54665e88a99bc8d3d1
Goodwin771 <ilchuk777ivan@gmail.com>
2023-12-25 16:32:39 +0000

    my first task from book JavaScript for beginners

---
+++ Задачки/Змінна розміру букв/index.js
@@ -0,0 +1,21 @@
+const mixedLetters = "hELlo THERE, hOW ARE yOu doING?";
+
+
+
+
+// const lowerCaseLetters = mixedLetters.toLowerCase();
+
+
+// const firstLowerLetter = lowerCaseLetters[0];
+
+// const firstUpperLetter = firstLowerLetter.toUpperCase();
+
+// const restOfString = lowerCaseLetters.slice(1);
+
+// const newString = firstUpperLetter+restOfString;
+
+// console.log(newString);
+
+const normalString = mixedLetters[0].toUpperCase() + mixedLetters.slice(1).toLowerCase();
+
+console.log(normalString);
